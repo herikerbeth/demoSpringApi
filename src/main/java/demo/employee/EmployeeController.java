c3bd8a50d13c88f18b48424b8cd54188003248ae
herikerbeth <herik.docarmo@hotmail.com>
2024-08-02 12:35:48 -0300

    First deploy

---
+++ src/main/java/demo/employee/EmployeeController.java
@@ -0,0 +1,13 @@
+package demo.employee;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/greeting")
+    public String hello() {
+        return "Hello World";
+    }
+}
